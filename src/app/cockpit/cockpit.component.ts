232e1c8bb3791c1cff921451aca2058bc6b4685e
Rahul Changlani <rahul.changlani@gmail.com>
2017-07-20 05:31:24 +0530

    local reference as param passed in method & ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,7 +11,10 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+  // @ViewChild(CockpitComponent) serverContentInput;
 
   constructor() { }
 
@@ -20,9 +23,10 @@ export class CockpitComponent implements OnInit {
 
   onAddServer(nameInput: HTMLInputElement) {
     console.log(nameInput.value);  // local reference
+    console.log(this.serverContentInput);  // local reference
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
@@ -30,7 +34,7 @@ export class CockpitComponent implements OnInit {
     console.log(nameInput.value);  // local reference    
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value    
     });
   }
 
